99d278fbfc36f827a6ece3b8b8e6956b13c05f82
cslim0527 <cslim0527@gmail.com>
2022-04-01 00:10:33 +0900

    각 필드별 상태 관찰 등록(register), 에러 핸들링, 옵션 설정, ref를 활용한 패스워드 확인방법, 제출 시 데이터 확인

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,77 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 import { useForm } from "react-hook-form";
+import { useRef } from "react";
 
 export default function Home() {
+  const {
+    register,
+    watch,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+  const password = useRef();
+  password.current = watch("password");
+
   return (
-      <form className={styles.form}>
-        <label>Example</label>
-        <input name="example" defaultValue="test"  />
-        <label>ExampleRequired</label>
-        <input
-            name="exampleRequired"
-            // ref={register({ required: true, maxLength: 10 })}
-        />
-        {/*{errors.exampleRequired && <p>This field is required</p>}*/}
-        <input type="submit" />
-      </form>
-  )
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <label>Email</label>
+      <input
+        name="email"
+        type="email"
+        {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+      />
+      {errors.email && <p>This email field is required</p>}
+
+      <label>Name</label>
+      <input
+        name="name"
+        type="text"
+        autoComplete="off"
+        {...register("name", { required: true, maxLength: 10 })}
+      />
+      {errors.name && errors.name.type === "required" && (
+        <p>This name field is required</p>
+      )}
+      {errors.name && errors.name.type === "maxLength" && (
+        <p>Your input exceed maximum length</p>
+      )}
+
+      <label>Password</label>
+      <input
+        name="password"
+        type="password"
+        autoComplete="new-password"
+        {...register("password", { required: true, minLength: 6 })}
+      />
+      {errors.password && errors.password.type === "required" && (
+        <p>This password field is required</p>
+      )}
+      {errors.password && errors.password.type === "minLength" && (
+        <p>Password must have at least 6 characters</p>
+      )}
+
+      <label>Confirm</label>
+      <input
+        name="password_confirm"
+        type="password"
+        autoComplete="new-password"
+        {...register("password_confirm", {
+          required: true,
+          validate: (value) => value === password.current,
+        })}
+      />
+      {errors.password_confirm &&
+        errors.password_confirm.type === "required" && (
+          <p>This password_confirm field is required</p>
+        )}
+      {errors.password_confirm &&
+        errors.password_confirm.type === "validate" && (
+          <p>The passwords do not match</p>
+        )}
+
+      <input type="submit" value="submit" />
+    </form>
+  );
 }
